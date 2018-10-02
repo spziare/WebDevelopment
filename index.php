43e8066f5d64453eecfb696a23722128fc0c246f
spziare <43794740+spziare@users.noreply.github.com>
2018-10-02 12:51:06 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	echo '<head>';
+	echo '<meta charset="UTF-8">';
+	echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
+	echo '<title>spziare</title>';
+	echo '</head>';
+?>
+
+<!--links-->
+	<head>
+		<link rel='icon' href='content/icon.png' type='image/x-icon'>
+		<!--css links-->
+		<link href='app/css/main.css' rel='stylesheet' type='text/css'>
+		<!--css links-->
+	</head>
+<!--links-->
